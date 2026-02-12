b3c001c0144a8fd0907d9adf91ddfd6bd816dd55
MrMobbi <julliatmathias06@gmail.com>
2026-02-12 15:02:41 +0100

    project Started

---
+++ incl/rubik.h
@@ -0,0 +1,8 @@
+#ifndef RUBIK_H
+# define RUBIK_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
---
+++ srcs/main.c
@@ -0,0 +1,10 @@
+
+#include "../incl/rubik.h"
+
+int main(int ac, char **av)
+{
+	(void) ac;
+	(void) av;
+	printf("Hello World\n");
+	return 0;
+}
